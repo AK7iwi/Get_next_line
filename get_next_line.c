23d719ef2a5e08c98b0e9fa1953094d62a8b9f5e
AK7iwi <atzlts@proton.me>
2022-10-22 00:02:27 +0200

    GNL fct + main vide

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,20 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:43:14 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/10/19 16:31:30 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/10/21 23:55:39 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char *get_next_line(int fd)
+{
+    char    *buf;
+    int     ret;
+    
+    buf = malloc(sizeof(*buf));
+    if (fd == -1)
+        return(NULL);
+    ret = read(fd, buf, )
+    free(buf);
+}
