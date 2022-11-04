037983bae1fbb72c35721e6d7cb6bd86de8785d3
AK7iwi <ATZLTS@proton.me>
2022-11-04 21:11:47 +0100

    First line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <michael.feldman.ca@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:43:14 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/11/04 17:53:57 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/11/04 21:11:01 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char *get_next_line(int fd)
     char    *buf;
     int     ret;
 
-    buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+    buf = malloc(sizeof(char) * (BUFSIZ + 1));
     if(!buf)
         return(NULL);
     if (fd < 0)
         return(NULL);
-    ret = read(fd, buf, BUFFER_SIZE);
+    ret = read(fd, buf, BUFSIZ);
     buf[ret] = '\0';
     return(SizeBuff(buf));   
 }
@@ -31,15 +31,20 @@ char *SizeBuff(char *buf)
 {
     static char *buf2;
     int i;
+    int j;
     
-    i = 0;
-    buf2 = malloc(sizeof(char) * BUFFER_SIZE + 1);
-    if(BUFFER_SIZE <= ft_lignelen(buf))
-        buf2[i] = buf[i];
-    if(BUFFER_SIZE > ft_lignelen(buf))
+    i = BUFSIZ;
+    j = 0;
+    buf2 = malloc(sizeof(char) * (BUFSIZ + 1));
+    if(i <= ft_lignelen(buf))
+        buf2 = buf;
+    if(i > ft_lignelen(buf))
     {
-        while(buf[i] && buf[i] != '\n')
-            buf2[i] = buf[i];
+        while(buf[j] && buf[j] != '\n')
+        {
+            buf2[j] = buf[j];
+            j++;
+        }
     }
     return(buf2);
 }
