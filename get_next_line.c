24f80f53ec20ff2bf3b0b6586ac7a757643b2293
AK7iwi <ATZLTS@proton.me>
2022-11-07 02:42:24 +0100

    Free

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mfeldman <michael.feldman.ca@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:43:14 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/11/05 01:44:35 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/11/07 02:28:10 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,27 @@
 
 char *get_next_line(int fd)
 {
-    char    *buf;
+    static char    *buf;
     int     ret;
 
     buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    ret = read(fd, buf, BUFFER_SIZE);
     if(!buf)
         return(NULL);
     if (fd < 0)
         return(NULL);
-    ret = read(fd, buf, BUFFER_SIZE);
     buf[ret] = '\0';
-    return(SizeBuff(buf));   
+    return(SizeBuff(buf)); 
+    free(buf);  
 }
 
 char *SizeBuff(char *buf)
 {
-    static char *buf2;
-    size_t i;
+    char *buf2;
     int j;
 
-    i = BUFFER_SIZE;
     j = 0;
     buf2 = malloc(sizeof(char) * (BUFFER_SIZE  + 1));
-    while (i < ft_lignelen(buf))
-        { 
-            buf2 = buf;
-            i += BUFFER_SIZE;
-        }
     while(buf[j] && buf[j] != '\n')
         {
             buf2[j] = buf[j];
@@ -48,3 +42,5 @@ char *SizeBuff(char *buf)
         }
     return(buf2);
 }
+
+
