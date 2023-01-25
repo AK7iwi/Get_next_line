264311b1e06efdf063c8ee81ad96faf43eebda63
AK7iwi <86202556+AK7iwi@users.noreply.github.com>
2023-01-25 02:05:02 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 19:47:57 by mfeldman          #+#    #+#             */
+/*   Updated: 2022/11/30 18:54:07 by mfeldman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_stock(char *stock, char *buf);
+int		ft_lignefin(char *buf);
+
+#endif 
