ee0473fc1309ba8cef683be44a974608ff299c38
AK7iwi <atzlts@proton.me>
2023-08-29 22:29:22 +0200

    Normed

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,13 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:25:12 by mfeldman          #+#    #+#             */
-/*   Updated: 2023/08/29 20:26:33 by mfeldman         ###   ########.fr       */
+/*   Updated: 2023/08/29 22:28:48 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /* Flags to use : -Wall -Wextra -Werror -I./includes/ */
-
 
 int	main(void)
 {
